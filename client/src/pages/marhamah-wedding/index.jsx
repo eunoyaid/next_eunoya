69e25952a7e424d99022fbad7ad595f3bfe44b67
Iko putro utomo <garudaputro@gmail.com>
2023-01-29 11:26:39 +0000

    added formik notif

--- client/src/pages/marhamah-wedding/index.jsx
+++ client/src/pages/marhamah-wedding/index.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import { Formik, Form, useField } from "formik";
 import * as Yup from "yup";
+import { Spinner, Toast } from "flowbite-react";
+import { HiCheck } from "react-icons/hi";
 
 const MyTextInput = ({ label, ...props }) => {
   // useField() returns [formik.getFieldProps(), formik.getFieldMeta()]
@@ -53,87 +55,78 @@ const MySelect = ({ label, ...props }) => {
 
 // And now we can use these
 const SignupForm = () => {
+  const [success, setSuccess] = useState();
   return (
     <>
       <h1>Subscribe!</h1>
       <Formik
         initialValues={{
           firstName: "",
-          lastName: "",
-          email: "",
           acceptedTerms: false, // added for our checkbox
-          jobType: "", // added for our select
         }}
         validationSchema={Yup.object({
           firstName: Yup.string()
             .max(15, "Must be 15 characters or less")
             .required("Required"),
-          lastName: Yup.string()
-            .max(20, "Must be 20 characters or less")
-            .required("Required"),
-          email: Yup.string()
-            .email("Invalid email address")
-            .required("Required"),
+
           acceptedTerms: Yup.boolean()
             .required("Required")
             .oneOf([true], "You must accept the terms and conditions."),
-          jobType: Yup.string()
-            .oneOf(
-              ["designer", "development", "product", "other"],
-              "Invalid Job Type"
-            )
-            .required("Required"),
         })}
         onSubmit={(values, actions) => {
-          // setTimeout(() => {
-          //   alert(JSON.stringify(values, null, 2));
-          //   setSubmitting(false);
-          //   actions.resetForm();
-          // }, 400);
-          console.log(actions);
-          actions.resetForm();
+          setTimeout(() => {
+            console.log(actions);
+            actions.resetForm();
+            setSuccess(true);
+          }, 400);
+          // console.log(actions);
+          // actions.resetForm();
+          // setSuccess(true)
         }}
       >
-        {(isSubmitting) => (
-          <Form>
-          <MyTextInput
-            label="First Name"
-            name="firstName"
-            type="text"
-            placeholder="Jane"
-          />
+        {(props) => {
+          const {
+            values,
+            touched,
+            errors,
+            dirty,
+            isSubmitting,
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            handleReset,
+          } = props;
+          return (
+            <Form>
+              {success && (
+                <Toast>
+                  <div className="inline-flex h-8 w-8 shrink-0 items-center justify-center rounded-lg bg-green-100 text-green-500 dark:bg-green-800 dark:text-green-200">
+                    <HiCheck className="h-5 w-5" />
+                  </div>
+                  <div className="ml-3 text-sm font-normal">
+                    Item moved successfully.
+                  </div>
+                  <Toast.Toggle />
+                </Toast>
+              )}
+              <MyTextInput
+                label="First Name"
+                name="firstName"
+                type="text"
+                placeholder="Jane"
+              />
+              <MyCheckbox name="acceptedTerms">
+                I accept the terms and conditions
+              </MyCheckbox>
 
-          <MyTextInput
-            label="Last Name"
-            name="lastName"
-            type="text"
-            placeholder="Doe"
-          />
-
-          <MyTextInput
-            label="Email Address"
-            name="email"
-            type="email"
-            placeholder="jane@formik.com"
-          />
-
-          <MySelect label="Job Type" name="jobType">
-            <option value="">Select a job type</option>
-            <option value="designer">Designer</option>
-            <option value="development">Developer</option>
-            <option value="product">Product Manager</option>
-            <option value="other">Other</option>
-          </MySelect>
-
-          <MyCheckbox name="acceptedTerms">
-            I accept the terms and conditions
-          </MyCheckbox>
-
-          <button type="submit" >
-          {isSubmitting  && ' sebentar'}
-            Submit</button>
-        </Form>
-        )}
+              <button type="submit">
+                
+                {isSubmitting && <Spinner aria-label="Default status example" />}
+                Submit
+              </button>
+            </Form>
+          );
+        }}
       </Formik>
     </>
   );
