f7136c549d662f2aef66edaca899bf34944eaf60
tomo <garudaputro@gmail.com>
2023-03-03 15:09:56 +0800

    added register feature

---
+++ api/config/plugins.js
@@ -0,0 +1,13 @@
+module.exports = ({env}) => ({
+  email: {
+    config: {
+      provider: 'strapi-provider-email-sendinblue',
+      providerOptions: {
+        sendinblue_api_key: env('SIB_API_KEY', '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'),
+        sendinblue_default_replyto: env('SIB_DEFAULT_REPLY_TO', 'contact@eunoya.id'),
+        sendinblue_default_from: env('SIB_DEFAULT_FROM', 'no-reply@eunoya.id'),
+        sendinblue_default_from_name: env('SIB_DEFAULT_FROM_NAME', 'Eunoya'),
+      },
+    },
+  },
+});
